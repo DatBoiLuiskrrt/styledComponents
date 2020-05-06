e8f02699b47e855c0d77ddd52b55a3e948519ca2
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-05-06 13:20:35 -0500

    Added some dependencies, also added some bottons to my header where I will be using icons that redirect to a new tab

--- components/src/App.js
+++ components/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../src/components/Header';
 
 function App() {
   return (
     <div >
-     <h1>Hola senor locutor</h1>
+     <Header />
     </div>
   );
 }
---
+++ components/src/components/Header.js
@@ -0,0 +1,102 @@
+import React from "react";
+import styled from "styled-components";
+
+const Nav = styled.div`
+  background-color: #fff;
+  border-bottom: 1px solid rgba(0,0,0,.0975);
+`
+const NavHeader = styled.div`
+  max-width: 1010px;
+  padding: 26px 20px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+`
+const NavLeft = styled.div`
+  width: 33.333%;
+  text-align: left;
+`
+const NavCenter = styled.div`
+  width: 33.333%;
+  text-align: center;
+`
+const Input = styled.input`
+  font-size: 16px;
+  border: solid 1px #dbdbdb;
+  border-radius: 3px;
+  color: #262626;
+  outline: 0;
+  padding: 7px 33px;
+  border-radius: 3px;
+  color: #999;
+  cursor: text;
+  font-size: 14px;
+  font-weight: 300;
+  text-align: center;
+  background: #fafafa;
+  &:active, &:focus {
+    text-align: left;
+  }
+`
+const NavRight = styled.div`
+  width: 33.333%;
+  text-align: right;
+  svg {
+    margin-right: 20px;
+  }
+`
+const MenuLink = styled.a`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  
+`
+const Titulo = styled.p`
+  color: red;
+
+`
+const Alert = () => {
+    return (
+       alert('You clicked Hello')
+    );
+};
+
+function Header () {
+    return (
+      <Nav>
+        <NavHeader>
+        
+          <NavLeft>
+            Instagram
+          </NavLeft>
+  
+          <NavCenter>
+            {/* <Input type="text" placeholder="Search" /> */}
+            <h1>Welcome Paisa 😜</h1>
+          </NavCenter>
+  
+          <NavRight>
+  
+            <MenuLink href="#">
+                <Titulo>Hello </Titulo>
+                <button onClick={ () => { Alert()}}>Hello</button>
+              {/* <Compass /> */}
+            </MenuLink>
+  
+            <MenuLink href="#">
+              {/* <Explore /> */}
+            </MenuLink>
+  
+            <MenuLink href="#">
+              {/* <Avatar /> */}
+            </MenuLink>
+            
+          </NavRight>
+  
+        </NavHeader>
+      </Nav>
+    )
+  }
+  
+  export default Header;
